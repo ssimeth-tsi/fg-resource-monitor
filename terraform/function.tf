114e241f45396581e77776f7c82550d3a9cebc6e
ssimeth <sandro.simeth@notitia-tech.de>
2025-08-12 09:18:49 +0200

    terraform create zip

--- terraform/function.tf
+++ terraform/function.tf
@@ -1,3 +1,28 @@
+# Using Terraform's archive_file data source
+data "archive_file" "function_zip" {
+  type        = "zip"
+  output_path = "${path.module}/../fg-resource-monitor.zip"
+  
+  source {
+    content  = file("${path.module}/../index.py")
+    filename = "index.py"
+  }
+  
+  source {
+    content  = file("${path.module}/../requirements.txt")
+    filename = "requirements.txt"
+  }
+  
+  # Add all Python files from src directory
+  dynamic "source" {
+    for_each = fileset("${path.module}/../src", "*.py")
+    content {
+      content  = file("${path.module}/../src/${source.value}")
+      filename = "src/${source.value}"
+    }
+  }
+}
+
 resource "opentelekomcloud_fgs_function_v2" "fg_resource_monitor" {
   name        = "fg-resource-monitor"
   app         = "default"
@@ -7,7 +32,7 @@ resource "opentelekomcloud_fgs_function_v2" "fg_resource_monitor" {
   timeout     = 30
   runtime     = "Python3.9"
   code_type   = "zip"
-  func_code   = filebase64("${path.module}/../fg-resource-monitor.zip")
+  func_code   = filebase64(data.archive_file.function_zip.output_path)
   
   lifecycle {
     ignore_changes = [user_data]
